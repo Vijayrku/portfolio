b6a07592e0d285c1525ce4c1bd30c4a17ff92fdf
Vijay <vijayrku@adobe.com>
2025-06-27 11:00:13 +0530

    Add new feature to user profile page
    Implemented additional functionality on the user profile page, allowing users to update their profile picture and bio. Refactored related components for better maintainability.

---
+++ src/blocks/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <a href="/">MyApp</a>
+      </div>
+      <ul className="navbar-menu">
+        <li><a href="/home">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
